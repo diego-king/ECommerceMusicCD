3e9b1bebd97eebb609311305ab43e787a0f38714
Diego <djin072@uottawa.ca>
2017-10-29 12:27:43 -0400

    Fix bug in check empty order.

--- CSI5380FrontEndMaven/src/com/store/controllers/OrderControllerServlet.java
+++ CSI5380FrontEndMaven/src/com/store/controllers/OrderControllerServlet.java
@@ -58,8 +58,8 @@ public class OrderControllerServlet extends HttpServlet {
 		String encodedPassword = (String) session.getAttribute("password");
 		String[] cdListArray = new String[] {};
 		String cdList = (String) session.getAttribute("cdList");
-		cdList = cdList.trim();
 		if (cdList != null) {
+			cdList = cdList.trim();
 			cdListArray = cdList.split(" ");
 		}
 		
